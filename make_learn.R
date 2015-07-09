d869c9ce277a2d6cd4e7ec968855d418642f0f2a
jules32 <jstewart@nceas.ucsb.edu>
2015-07-09 15:13:09 +0100

    reorg folders, rename to conceptual to learn_concguide

--- make_learn.R
+++ make_learn.R
@@ -1,44 +1,20 @@
 # Ocean Health Index, Santa Barbara, CA
-# last updated: 05 May 2015
-
-##### Summary: the goal of this code is to combine the text in the `conceptual` folder into one markdown document for reading and editing purposes.
-
-# Step A. Loading Libraries, Directories, Functions:
-# Note: this code is mainly copied form `make.R` but changed all instances of variables to "v2" just in case.
+# last updated: 08 July 2015
 
 # load libraries ----
-library(knitr) # install.packages("rmarkdown")
+library(knitr) # install.packages("knitr")
 library(rmarkdown)
 library(stringr)
 
-# set working directory 
-getwd()
-here <- "./conceptual" #creating variable for working directory - can change this
-setwd('./conceptual')
-getwd() #double checking
-
 # set variables ----
-titlev2 = 'The Ocean Health Index Conceptual Guide'
-
-# helper functions ----
-cat_mdv2 = function(
-  files_mdv2 = setdiff(list.files(getwd(), glob2rx('*.md')), out_mdv2),
-  out_mdv2  = '_all_.mdv2'){
-  
-  if (file.exists(out_mdv2)) unlink(out_mdv2)
-  
-  cat('---\n', 'title: ', titlev2, '\n---\n\n', sep='', file=out_mdv2, append=T)
-  
-  for (md in files_mdv2){
-    cat(paste(c(readLines(md),'',''), collapse='\n'), file=out_mdv2, append=T)
-  }
-}
+title = 'The Ocean Health Index Conceptual Guide'
+wd = '~/github/ohimanual/learn_concguide'
 
 
 # Step B. This is the order of contents. Change it as needed, noting that these files must be in this directory for the time being. If we want to string 'content' and 'conceptual' together, we'll have to change the wd's.
 # note: to seam in pages from `content`, use the following format below:   '~/github/ohimanual/content/AO_practical.md',
 
-in_mdv2 = c(
+in_md = c(
   'Phase_0.md',
   #'Need_For_and_Benefits_of_OHI.md', # --> See Phase_0
   #'Task_Timeline.md', # --> See Phase_0
@@ -93,32 +69,24 @@ in_mdv2 = c(
   '4_Monitor_and_Re-evaluate_to_Determine_Policy_Effectiveness_and_Set_New_Priorities.md',
   '0_glossary.md')
 
-out_mdv2 = 'ohi-concguide.md' # <- this is the output "final" file.
-
-#### testing ground
-# playing around with a way to autmate this....  failed: toc <- list.files('.')
-#ch1 <- "Adaptive_Management.md"
-#ch12 <- c("Adaptive_Management.md","AO_Philosophy.md", recurusive = TRUE)
-#knit("Adaptive_Management.md", "output.md", text = NULL)  #test1
-#knit(ch1, "output2.md", text = NULL) #test2 works
-#knit("Adaptive_Management.md","AO_Philosophy.md", "output3.md", text = TRUE) #test3 breask
+out_md = 'ohi-concguide.md' # <- this is the output "final" file.
 
 
 
 # Step C. This is where the output gets strung together, here into both .md and .html for convenience.
 
-cat_mdv2(in_mdv2, out_mdv2)               # use own md ordered file listing , output to ohi-manual.md
-pfx = tools::file_path_sans_ext(out_mdv2)
+cat_md(in_md, out_md)               # use own md ordered file listing , output to ohi-manual.md
+pfx = tools::file_path_sans_ext(out_md)
 
 # This is the final step to output to either a .md file for now. Next step: use the complex "render_html" etc. for website styling...
-knit(out_mdv2, "ohi-concguide.md", text = NULL)
+knit(out_md, "ohi-concguide.md", text = NULL)
 
 knit2html('ohi-concguide.md', 'ohi-concguide.html') # update: this is easier to read, IMHO, so I'll render this output to html too while we're at it...
 
 
 # render pdf ----
 render(
-  out_mdv2,
+  out_md,
   pdf_document(
     toc = T, toc_depth = 4, number_sections = T,
     fig_width = 6.5, fig_height = 4.5, fig_crop = TRUE,
@@ -128,5 +96,17 @@ render(
   clean=T, quiet=F,
   output_file = paste0(pfx, '.pdf'))
 
-# Step D. Continue editing!
-# test
+
+# helper functions ----
+cat_md = function(
+  files_md = setdiff(list.files(getwd(), glob2rx('*.md')), out_md),
+  out_md  = '_all_.md'){
+  
+  if (file.exists(out_md)) unlink(out_md)
+  
+  cat('---\n', 'title: ', title, '\n---\n\n', sep='', file=out_md, append=T)
+  
+  for (md in files_md){
+    cat(paste(c(readLines(md),'',''), collapse='\n'), file=out_md, append=T)
+  }
+}
